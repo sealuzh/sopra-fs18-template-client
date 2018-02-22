e86daacc05c3d6b713d768e7685953879719efa4
Sali Zumberi <sali.zumberi@live.de>
2018-02-22 21:44:32 +0100

    summarize everything in your app module
    app module is like an index,
    you define here everything which you want to use later in the applications
    
    external angular modules, your components, services, pipes etc..
    
    read more here: https://v2.angular.io/docs/ts/latest/guide/appmodule.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {GameComponent} from './game/game.component';
+import {LoginComponent} from './login/login.component';
+import {UserService} from './shared/services/user.service';
+import {AuthGuardService} from './shared/services/auth-guard.service';
+import {AuthenticationService} from './shared/services/authentication.service';
+import {routing} from './app.routing';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routing
   ],
-  providers: [],
+  providers: [AuthenticationService, AuthGuardService, UserService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
